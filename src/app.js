e5f667cd28982b56f9be43c0279fbe08949eb679
kumar Sah <abhishek.kumarsah@adidas.com>
2026-02-08 17:01:46 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,16 +1,36 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-// THis will only handle GET API call to /user
-app.get("/user/:userID/:name", (req,res) =>{
-    console.log(req.params);
-    console.log(req.query);
-    res.send({firtsName: "Abhishek", lastName: "sah"});
-});
+//route handler for all API calls to /user
+// we can use inside app.get, app.post, app.put etc.
+
+// app.use("/route", RequestHandler1, [RequestHandler2, RequestHandler3], ...)
+
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the request for /user 1");
+    // next();
+    res.send("Response 1");
+    next();
+  },
+  [
+    (req, res, next) => {
+      console.log("Handling the request for /user 2");
+      res.send("Response 2");
+    },
+    (req, res, next) => {
+      console.log("Handling the request for /user 3");
+      res.send("Response 3");
+    },
+  ],
+  (req, res, next) => {
+    console.log("Handling the request for /user 4");
+    res.send("Response 4");
+  },
+);
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
-
-
