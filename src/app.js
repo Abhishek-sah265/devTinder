0b61008bdd9b50b0d3a00be8dcf5a3dacc45cf66
kumar Sah <abhishek.kumarsah@adidas.com>
2026-02-08 16:44:13 +0530

    reading dynamic route

--- src/app.js
+++ src/app.js
@@ -3,29 +3,11 @@ const express = require('express');
 const app = express();
 
 // THis will only handle GET API call to /user
-app.get("/user", (req,res) =>{
+app.get("/user/:userID/:name", (req,res) =>{
+    console.log(req.params);
+    console.log(req.query);
     res.send({firtsName: "Abhishek", lastName: "sah"});
 });
-
-app.post("/user", (req,res) =>{ 
-    res.send({message: "Data is saved successfully"});
-});
-
-app.delete("/user", (req,res) =>{
-    res.send({message: "Data is deleted successfully"});    
-});
-//This will match all the HTTP Methods(GET, POST, PUT, DELETE) API call to /test
-app.use("/test",(req,res) =>{
-    res.send("Hi from the server");
-});
-
-// app.use("/hello",(req,res) =>{
-//     res.send("hello hello hello the server");
-// });
-
-// app.use("/",(req,res) =>{
-//     res.send("Hi from the dashboard of dev tinder");
-// });
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
