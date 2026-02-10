7dd9d0f99add4a0e1bd2ef87955dbdc60a4bb1c2
kumar Sah <abhishek.kumarsah@adidas.com>
2026-02-10 12:13:26 +0530

    handling errors

--- src/app.js
+++ src/app.js
@@ -1,29 +1,27 @@
 const express = require("express");
-const { adminAuth, userAuth } = require("./middleware/auth");
 const app = express();
 
-// Request  →  middleware(s)  →  route handler  →  Response
-
-app.use("/admin", adminAuth);
-// app.use("/user", userAuth); // we can also apply middleware like this
-
-app.post("/user/login"), (req,res) => {
-    res.send("User logged in successfully");
-}
-
-// because we have only route for user then we can also apply middleware like this
-app.get("/user", userAuth, (req, res, next) => {
-  res.send("User route accessed successfully");
+// the order of this middleware matters, we should define it after all routes not in the start.
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Internal Server Error");
+  }
 });
 
+app.get("/user", (req, res) => {
+  try {
+    throw new Error("Simulated server error");
+    res.send("User route accessed successfully");
+  } catch (err) {
+    res.status(500).send("Try catch Error");
+  }
+});
 
-app.use("/admin/getAllData", (req,res,next) => {
-    res.send("All data sent successfully");
-})
-
-app.use("/admin/deleteData", (req,res,next) => {
-    res.send("Deleted data successfully");
-})
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
